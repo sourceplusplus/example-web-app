dec43e1372d4cd9791ff6f51abb9b274882adcef
Brandon Fergerson <bfergerson@apache.org>
2022-07-21 15:34:07 +0400

    add test

---
+++ src/main/java/spp/example/webapp/test/InstrumentTest.java
@@ -0,0 +1,39 @@
+package spp.example.webapp.test;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class InstrumentTest implements Runnable {
+
+    private final PrintStream nullStream = new PrintStream(new OutputStream() {
+        public void write(int b) {
+        }
+    });
+
+    @PostConstruct
+    public void init() {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void run() {
+        int i = 1;
+        char c = 'h';
+        String s = "hi";
+        float f = 1.0f;
+        long max = Long.MAX_VALUE;
+        byte b = -2;
+        short sh = Short.MIN_VALUE;
+        double d = 00.23d;
+        boolean bool = true;
+        nullStream.print(i + c + s + f + max + b + sh + d + bool);
+    }
+}
